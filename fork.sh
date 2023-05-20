8c12874a8d2866239968bd5508446f6a3c5d9196
Sheeza-Aslam <134129196+Sheeza-Aslam@users.noreply.github.com>
2023-05-20 05:06:46 -0400

    Uploaded fork.sh

--- fork.sh
+++ fork.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-echo "parent process.."
+echo "parent process.. $$"
 
-{
-    sleep 2
-    echo "child process.."
-} &
+if child_pid=$(fork); then
+    if [ "$child_pid" -eq 0 ]; then
+        echo "child process.. $$"
+    fi
+else
+    echo "failed to fork.."
+    exit 1
+fi
 
 sleep 2
+
+if [ "$child_pid" -eq 0 ]; then
+    echo "child process resumes after sleep"
+else
+    echo "parent process resumes after sleep"
+fi
+
+exit 0
